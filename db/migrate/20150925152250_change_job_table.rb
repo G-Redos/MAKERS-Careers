566f3be7f3e63b1ac737c426ab461c07e6ed4461
Becks Hookham <becks@makersacademy.com>
2022-09-26 14:19:20 +0100

    Delete db directory

--- db/migrate/20150925152250_change_job_table.rb
+++
@@ -1,8 +0,0 @@
-class ChangeJobTable < ActiveRecord::Migration
-  def change
-    remove_column :jobs, :company
-    rename_column :jobs, :name, :company
-    rename_column :jobs, :description, :summary
-    add_column :jobs, :specification, :text
-  end
-end
--- db/migrate/20150929165517_change_job_applicationto_job_interest.rb
+++
@@ -1,5 +0,0 @@
-class ChangeJobApplicationtoJobInterest < ActiveRecord::Migration
-  def change
-    rename_table :job_applications, :job_interests
-  end
-end
