c6bc3d0c794907f1f218b7193380911d760c1c8c
Becks Hookham <becks@makersacademy.com>
2022-09-27 12:31:49 +0100

    Delete spec directory

--- spec/models/job_interest_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe JobInterest do
-  it { is_expected.to belong_to(:job) }
-  it { is_expected.to belong_to(:student) }
-end
--- spec/models/student_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Student, type: :model do
-  it { is_expected.to have_many(:jobs).through(:job_interests) }
-end
